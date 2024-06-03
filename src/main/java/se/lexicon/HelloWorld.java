6b7b5ae94df798cc6e5bd5681762363eda52e14f
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-06-03 09:40:51 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ */
+public class HelloWorld {
+
+    // this is a single-line comment
+
+    /*
+    this is a multi-line comment
+    it can span multiple lines
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
